fe6c00b5e47d2c24c1975db1c4e869df5c999ea3
nicoBru22 <brunet.nicolas35@gmail.com>
2025-08-01 18:57:30 +0200

    test suivant

--- Back/microService-patient/src/main/java/com/medilabo/controller/HealthCheckController.java
+++ Back/microService-patient/src/main/java/com/medilabo/controller/HealthCheckController.java
@@ -2,9 +2,11 @@ package com.medilabo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/patient")
 public class HealthCheckController {
 
  @GetMapping("/health") // Ou n'importe quel autre chemin non utilisé
